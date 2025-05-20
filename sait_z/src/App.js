27bad68fc2b602439890c00b14ea0648a6029979
Pavel <pavel@b2profit.ru>
2025-05-20 18:29:01 +0300

    Цвет хэдера

--- sait_z/src/App.js
+++ sait_z/src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+
 
 function App() {
   return (
     <div className="App">
+       <Router>
+          <Routes>
+            <Route path="*" element={<HomePage/>} />
+            <Route path="/" element={<HomePage />} />
+            {/* <Route path="/products" element={<Products />} /> */}
+            {/* <Route path="/lecturers" element={<Lecturers />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="/lesson" element={<Lesson />} />
+            <Route path="/info" element={<Info />} /> */}
+          </Routes>
+        </Router>
      
     </div>
   );
---
+++ sait_z/src/components/HomePage/HomePage.js
@@ -0,0 +1,43 @@
+import Header from "../Header";
+import Typography from '@mui/material/Typography';
+
+function HomePage() {
+
+    return (
+        <div>
+            <Header />
+            <div className="pages center">
+                <Typography className='pages__title' variant="h4" gutterBottom>
+                    Похоронное бюро Сергея Завьялова
+                </Typography>
+
+                {/* <div className='pages__title'>
+                    <p>
+                        ИТ-специалисты востребованы в финансах, ритейле, образовании и многих других отраслях.
+                    </p>
+                    <p>
+                        Экономика и повседневная жизнь все больше переходит в «цифру»,
+                    </p>
+                    <p>
+                        поэтому у ИТ-сферы самые большие перспективы на развитие в ближайшем будущем.
+                    </p>
+                </div> */}
+
+                {/* <div className='box'>
+                    <a href="/programs" >
+                        <ProgramCart id={'title-banner'} img={require('../../img/title_banner.jpg')} title={'Выберите программу обучения'} text={'Перейдите в раздел "Программы обучения". Выберите  интересующее вас направление, при помощи фильтра и начните обучение.'} />
+                    </a>
+                    <a href="/lecturers" >
+                        <ProgramCart id={'title-banner'} img={require('../../img/title_banner_2.jpg')} title={'Познакомьтесь с преподавателями'} text={'Перейдите в раздел "Преподаватели". Прочитайте подробную информацию о наших преподавателях.'} />
+                    </a>
+                </div> */}
+
+            </div>
+            {/* <Footer /> */}
+
+        </div >
+
+    );
+}
+
+export default HomePage;
