30e21850f77b7be8639cefc79e9ccf064b99b4b3
Pavel <pavel@b2profit.ru>
2025-08-05 18:05:13 +0300

    Поместил каталог из pdf в инфо

--- sait_z/src/components/Info/Info.js
+++ sait_z/src/components/Info/Info.js
@@ -1,17 +1,25 @@
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Typography from '@mui/material/Typography';
-import faq from '../../data/faq';
+// import Accordion from '@mui/material/Accordion';
+// import AccordionSummary from '@mui/material/AccordionSummary';
+// import AccordionDetails from '@mui/material/AccordionDetails';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import Typography from '@mui/material/Typography';
+// import faq from '../../data/faq';
 import Footer from "../Footer";
 import Header from "../Header";
 
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
+
 function Info() {
+
+
     return (
         <div>
             <Header services='white' products='white' info='primary' />
-            <div className="pages  ">
+
+
+            {/* <div className="pages  ">
                 <div className='box'>
                     <div className="pages center">
                         <Typography className='pages__title' variant="h4" gutterBottom>
@@ -35,10 +43,18 @@ function Info() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>  */}
+
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+               <Viewer fileUrl={require('../../img/goods_1.pdf')} />;
+            </Worker>      
+
+
+
+
             <Footer />
 
-        </div>
+        </div >
     );
 }
 
