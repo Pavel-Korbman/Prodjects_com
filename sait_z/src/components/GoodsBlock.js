01fd89d7f72dac6d974c87677b75ec6cac69ca97
Pavel <pavel@b2profit.ru>
2025-09-05 15:16:08 +0300

    Заменил файл ритуальной одежды

--- sait_z/src/components/GoodsBlock.js
+++ sait_z/src/components/GoodsBlock.js
@@ -9,63 +9,63 @@ function GoodsBlock() {
             <nav className="type__block on">
                 <a href="/cloth-coffins">
                     <div className="type__item type__item__bgrd-10">
-                        <p className="type__item__text-1">Гробы обитые тканью и комбинированные</p>
+                        <p className="type__item__text-10">Гробы обитые тканью и комбинированные</p>
                         <p className="type__item__text">5500₽ - 22000₽</p>
                     </div>
                 </a>
 
                 <a href="/polished-coffins">
                     <div className="type__item type__item__bgrd-20">
-                        <p className="type__item__text-1">Гробы полированные и фурнитура</p>
+                        <p className="type__item__text-10">Гробы полированные и фурнитура</p>
                         <p className="type__item__text">19900₽ - 430000₽</p>
                     </div>
                 </a>
 
                 <a href="bedding">
                     <div className="type__item type__item__bgrd-30">
-                        <p className="type__item__text-1">Постель в гроб</p>
+                        <p className="type__item__text-10">Постель в гроб</p>
                         <p className="type__item__text">600₽ - 13540₽</p>
                     </div>
                 </a>
 
                 <a href="dress">
                     <div className="type__item type__item__bgrd-40">
-                        <p className="type__item__text-1">Ритуальная одежда и траурные принадлежности</p>
+                        <p className="type__item__text-10">Ритуальная одежда и траурные принадлежности</p>
                         {/* <p className="type__item__text"></p> */}
                     </div>
                 </a>
 
                 <a href="crosses">
                     <div className="type__item type__item__bgrd-50">
-                        <p className="type__item__text-1">Кресты и ритуальные таблички</p>
+                        <p className="type__item__text-10">Кресты и ритуальные таблички</p>
                         <p className="type__item__text">3000₽ - 46000₽</p>
                     </div>
                 </a>
 
                 <a href="garlands">
                     <div className="type__item type__item__bgrd-60">
-                        <p className="type__item__text-1">Гирлянды в изголовье</p>
+                        <p className="type__item__text-10">Гирлянды в изголовье</p>
                         <p className="type__item__text">1140₽ - 1950₽</p>
                     </div>
                 </a>
 
                 <a href="compositions">
                     <div className="type__item type__item__bgrd-70">
-                        <p className="type__item__text-1">Кремационные композиции</p>
+                        <p className="type__item__text-10">Кремационные композиции</p>
                         <p className="type__item__text">2400₽ - 11700₽</p>
                     </div>
                 </a>
 
                 <a href="wreaths">
                     <div className="type__item type__item__bgrd-80">
-                        <p className="type__item__text-1">Венки и ленты</p>
+                        <p className="type__item__text-10">Венки и ленты</p>
                         <p className="type__item__text">3025₽ - 19000₽</p>
                     </div>
                 </a>
 
                 <a href="cars">
                     <div className="type__item type__item__bgrd-90">
-                        <p className="type__item__text-1">Катафальный транспорт</p>
+                        <p className="type__item__text-10">Катафальный транспорт</p>
                         {/* <p className="type__item__text"></p> */}
                     </div>
                 </a>  
